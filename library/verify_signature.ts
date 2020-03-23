64d81b5723d5e14a35f787870536827e9e1399cc
Adam Sinnett <quandrum@gmail.com>
2020-03-22 21:33:31 -0700

    Use same scale of timestamp

--- library/verify_signature.ts
+++ library/verify_signature.ts
@@ -1,6 +1,6 @@
 import hmac from 'hmac'
 
-const FIVE_MINUTES = 1000 * 60 * 5
+const FIVE_MINUTES = 60 * 5
 
 export default ({ body, headers }) => {
   if (process.env.DEV) {
@@ -9,7 +9,8 @@ export default ({ body, headers }) => {
 
   // Allow five minute drift between sending and receiving, otherwise probably replay attack
   const timestamp = headers['X-Slack-Request-Timestamp']
-  const elapsed = Math.abs(Date.now() - timestamp)
+  const now = Date.now() / 1000
+  const elapsed = Math.abs(now - timestamp)
   if (elapsed > FIVE_MINUTES) {
     return `elapsed ${elapsed} (from ${timestamp}) is off more than five minutes ${FIVE_MINUTES}`
   }
